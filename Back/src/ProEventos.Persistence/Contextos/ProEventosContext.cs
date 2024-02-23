bada8f87d1c09d4ab759e89d3d77e3fecc318ae5
Leonardo Urbano <leonardourbano244@gmail.com>
2024-02-23 17:21:05 -0300

    Seção 6: .NET Core - Organizando Camadas

---
+++ Back/src/ProEventos.Persistence/Contextos/ProEventosContext.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Contextos
+{
+    public class ProEventosContext : DbContext
+    {
+        public ProEventosContext(DbContextOptions<ProEventosContext> options) : base(options) {}
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Lote> Lotes  { get; set; }
+        public DbSet<Palestrante> Palestrantes  { get; set; }
+        public DbSet<PalestranteEvento> PalestrantesEventos  { get; set; }
+        public DbSet<RedeSocial> RedesSociais  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PalestranteEvento>()
+                        .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
+
+            modelBuilder.Entity<Evento>()
+                        .HasMany(e => e.RedesSociais)
+                        .WithOne(rs => rs.Evento)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Palestrante>()
+                        .HasMany(e => e.RedesSociais)
+                        .WithOne(rs => rs.Palestrante)
+                        .OnDelete(DeleteBehavior.Cascade);
+
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                        .SelectMany(t => t.GetProperties())
+                        .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+            {   
+              property.SetColumnType("decimal(18,2)");   
+            }
+        }
+    }
+}
