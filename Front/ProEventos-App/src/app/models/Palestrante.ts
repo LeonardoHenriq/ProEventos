36677f75a37787a3c07e97c60794333ccf9ed16f
Leonardo Urbano <leonardourbano244@gmail.com>
2024-02-27 10:10:26 -0300

    Seção 7: Angular - Organizando, Rota, Alerta e +

---
+++ Front/ProEventos-App/src/app/models/Palestrante.ts
@@ -0,0 +1,13 @@
+import { EventosComponent } from "../components/eventos/eventos.component";
+import { RedeSocial } from "./RedeSocial";
+
+export interface Palestrante {
+  id: number;
+  nome: string;
+  miniCurriculo: string;
+  imagemURL: string;
+  telefone: string;
+  email: string;
+  redesSociais: RedeSocial[];
+  palestrantesEventos: EventosComponent[];
+}
